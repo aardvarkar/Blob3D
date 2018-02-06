5695a9dfd75aa5358c186c8436d473abf12e54ae
Martin Binet <martin.binet@laposte.net>
2018-02-06 10:19:24 +0100

    clean node.js

--- Serveur/subpart/metabolism/js/reload.js
+++
@@ -1,8 +0,0 @@
-function reloadCanvas() {
-  var cy = window.cy;
-  var layout = cy.layout({
-      name: 'cose'
-  });
-  layout.run();
-  console.log('reload');
-}
--- Serveur/subpart/proteins/ngl_script.js
+++
@@ -1,6 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//
-// });
-
-var stage = new NGL.Stage("viewport");
-var listeProts = [];
