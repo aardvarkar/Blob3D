9d5e29a0f769b72bfb4e5cf1384bfd60d973cec0
aardvarkar <adrien.rohan@hotmail.fr>
2018-02-10 20:01:53 +0100

    update

--- Adrien/Server.js
+++ Adrien/Server.js
@@ -1,95 +1,76 @@
-var spawn   = require('child_process').spawn;
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
+let spawn = require('child_process').spawn;
+let express = require("express");
+let path = require("path");
+let fs = require("fs");
+//let FileReader = require('filereader'); //Ajout
 
 app = express();
-// Ajout
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); 
+
+
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public/js'));
 
-app.get("/acceuil", (req, res) => {
+app.get("/", (req, res) => {
     console.log("Welcome");
     res.sendFile(__dirname + "/public/index.html");
 })
 
-app.get("/subpart/proteins", (req, res) => {
-            console.log("Proteins")
-            var files = fs.readdir(__dirname + "/public", (err, files) => {
-            fs.writeFile(__dirname + "/public/test.txt", files.map(x =>  x));
-            })
 
-            res.sendFile(__dirname + "/public/subpart/proteins/proteins.html");
+fs.readdir(__dirname + "/public/subpart/proteins/PDB/PDB_BLOB", (err, files) => {
+  fs.writeFile(__dirname + "/public/subpart/proteins/listPDBServer.txt", files.map(x =>  x));
 })
-	
 
 app.post("/subpart/proteins", (req, res) => {
-    var dir = __dirname + '/public/subpart/proteins/PDB/tmpFolder';
+    console.log("in server");
+    let dir = __dirname + '/public/subpart/proteins/PDB/' + req.body['nomprojet'];
     if (!fs.existsSync(dir)){
 	fs.mkdirSync(dir);
+	//
+	console.log(typeof req.body['pdbFiles1']);
+	//var readerPdb1 = new FileReader();
+	/*readerPdb1.addEventListener("loadend", function() {
+	    console.log(readerPdb1.result);
+	});
+	readerPdb1.readAsText(req.body['pdbFiles1']);*/
+	//var pdbFile1 = new File([req.body['pdbFiles1']], filename, {type: contentType, lastModified: Date.now()});
+	//var pdbFile2 = new File([req.body['pdbFiles2']], filename, {type: contentType, lastModified: Date.now()});
+	//
+	let macroPath =  __dirname + '/public/subpart/proteins/PDB/' + req.body['nomprojet'] + '/macro.mac';
+	let line1 = "open_receptor " + __dirname + "/public/subpart/proteins/PDB/PDB_BLOB/" + req.body['prot1'] + "\n";
+	let line2 = "open_ligand " + __dirname + "/public/subpart/proteins/PDB/PDB_BLOB/" + req.body['prot2'] + "\n";
+	let line3 = "activate_docking\n";
+	let line4 = "save_both " + __dirname + "/public/subpart/proteins/PDB/" + req.body['nomprojet'] + "//resultDocking.pdb\n";
+	let line5 = "exit\n";
+	let logger = fs.createWriteStream(macroPath, {
+  	    flags: 'w'
+	})
+	logger.write(line1);
+	logger.write(line2);
+	logger.write(line3);
+	logger.write(line4);
+	logger.write(line5);
+	logger.end();
+	console.log("N passage");
+	let command ="bash " +  __dirname + "/public/subpart/proteins/hex/bin/hex -e " + macroPath;
+	const exec = require('child_process').exec;
+	let yourscript = exec(command,
+  			      (error, stdout, stderr) => {
+  				  console.log(`${stdout}`);
+  				  console.log(`${stderr}`);
+  				  if (error !== null) {
+  				      console.log(`exec error: ${error}`);
+  				  }
+  			      });
+        res.send( {msg: 'Success'});
     }
-    var macroPath =  __dirname + '/public/subpart/proteins/PDB/tmpFolder/macro.mac';
-    var line1 = "open_receptor " + __dirname + "/public/subpart/proteins/PDB/PDB_BLOB/" + req.body['prot1'] + "\n";
-    var line2 = "open_ligand " + __dirname + "/public/subpart/proteins/PDB/PDB_BLOB/" + req.body['prot2'] + "\n";
-    var line3 = "activate_docking\n";
-    var line4 = "save_both " + __dirname + "/public/subpart/proteins/PDB/tmpFolder//docking_" + req.body['prot1'].substr(0,req.body['prot1'].length-4) + "_" + req.body['prot2'].substr(0,req.body['prot2'].length-4) + ".pdb\n";
-    var line5 = "exit\n";
-    var logger = fs.createWriteStream(macroPath, {
-	flags: 'w'
-    })
-    logger.write(line1);
-    logger.write(line2);
-    logger.write(line3);
-    logger.write(line4);
-    logger.write(line5);
-    logger.end();
-    console.log("N passage");
-    var command ="bash " +  __dirname + "/public/subpart/proteins/hex/bin/hex -e " + macroPath;
-    const exec = require('child_process').exec;
-    var yourscript = exec(command,
-			  (error, stdout, stderr) => {
-			      console.log(`${stdout}`);
-			      console.log(`${stderr}`);
-			      if (error !== null) {
-				  console.log(`exec error: ${error}`);
-			      }
-			  });
-    //res.send('Success');
-    /*
-    const exec = require('child_process').exec;
-    var tmpcommand = "bash " + __dirname + "/public/subpart/proteins/commandline1.sh";
-    var yourscript = exec(tmpcommand,
-			  (error, stdout, stderr) => {
-			      console.log(`${stdout}`);
-			      console.log(`${stderr}`);
-			      if (error !== null) {
-				  console.log(`exec error: ${error}`);
-			      }
-			      });*/
+    else{
+	res.send({ msg:'This project name already exists!'});
+    }
 })
-/*
-const exec = require('child_process').exec;
-console.log(__dirname);
-var tmpcommand = "bash " + __dirname + "/public/subpart/proteins/commandline1.sh";
-var yourscript = exec(tmpcommand,
-        (error, stdout, stderr) => {
-            console.log(`${stdout}`);
-            console.log(`${stderr}`);
-            if (error !== null) {
-                console.log(`exec error: ${error}`);
-            }
-        });*/
-    
 
-//var file = __dirname + '/upload-folder/dramaticpenguin.MOV';
-//res.download(file);
-console.log('HalfSuccess');
-
-
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
---
+++ Adrien/proteins.js
@@ -0,0 +1,295 @@
+var fileFromServer;
+
+let current_representation = 'cartoon';
+let current_scheme = 'chainid';
+let spin_flag = false;
+
+function initialLoad(){
+    var xhttp = new XMLHttpRequest();
+}
+
+function loadPDB(file){
+  stage.loadFile(file, {defaultRepresentation: true}).then( function( comp){
+      console.log("loading successful");
+      listeProts.push(comp);
+      listeBlobs.push(file); //Ajout
+      changeRepresentation(current_representation, current_scheme);
+      addChoice("mySelect1");
+      addChoice("mySelect2");
+      console.log(file.getAsText("utf8")); //Ajout
+  });
+}
+
+function getFile(){
+    var doc = document.forms["proteinList"].elements["selection"];
+    var pdb = doc[doc.selectedIndex].text;
+
+    if(pdb == "Browse for a new file..."){
+	var elem = document.getElementById("hiddenInput");
+	var evt = document.createEvent("MouseEvents");
+	evt.initEvent("click", true, false);
+	elem.dispatchEvent(evt);
+    }
+    else{
+	console.log("loading file from server...");
+	getFileFromServer("PDB/PDB_BLOB/"+pdb, function(file) {
+	    //alert(typeof file); // Ajout
+            if (file === null) {
+		alert("File could not be found. Try again later.");
+            }
+            else {
+		file.lastModifiedDate = new Date();
+		file.name = pdb;
+		loadPDB(file);
+            }
+	});
+    }
+
+
+}
+
+function getFileFromServer(url, doneCallback) {
+
+    fileFromServer = new XMLHttpRequest();
+    fileFromServer.onreadystatechange = handleStateChange;
+    fileFromServer.open("GET", url, true);
+    fileFromServer.responseType = 'blob';
+    fileFromServer.send();
+
+    function handleStateChange() {
+        if (fileFromServer.readyState === 4) {
+            doneCallback(fileFromServer.status == 200 ? fileFromServer.response : null);
+        }
+    }
+}
+
+function addChoice(selectName){
+    var dropdownSelected = document.getElementById(selectName);
+    var option = document.createElement("option");
+    option.class = "option";
+    option.text = listeProts[listeProts.length-1].name;
+    dropdownSelected.add(option);
+}
+
+function clearChoice(selectName){
+    dropdownSelected = document.getElementById(selectName);
+    while (dropdownSelected.children[1]) {
+        dropdownSelected.removeChild(dropdownSelected.children[1]);
+    }
+}
+
+function changeRepresentation(representation, current_scheme){
+    stage.eachComponent(function( o ){
+        o.removeAllRepresentations();
+        o.addRepresentation( representation, {
+                                              sele: "polymer",
+                                              colorScheme: current_scheme,
+                                              colorDomain: [ -0.3, 0.3 ],
+                                              surfaceType: "av"
+                                            });
+        current_representation = representation;
+    });
+}
+
+function changeColor(scheme, current_representation){
+    stage.eachComponent(function( o ){
+        o.removeAllRepresentations();
+        o.addRepresentation( current_representation, {
+                                              sele: "polymer",
+                                              colorScheme: scheme,
+                                              colorDomain: [ -0.3, 0.3 ],
+                                              surfaceType: "av"
+                                            });
+        current_scheme = scheme;
+    });
+
+}
+
+function clear(){
+    stage.removeAllComponents();
+    listeProts = [];
+    listeBlobs = []; //Ajout
+    clearChoice("mySelect1");
+    clearChoice("mySelect2");
+}
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+document.addEventListener("load", initialLoad());
+
+function dock(){    
+    var prot1 = document.getElementById("mySelect1")
+    var prot1pdb = prot1.options[prot1.selectedIndex].text
+
+    var prot2 = document.getElementById("mySelect2")
+    var prot2pdb = prot2.options[prot2.selectedIndex].text
+
+    if (prot1pdb === "Select first protein..." || prot2pdb === "Select second protein..."){
+        alert("Please select two protein files for docking.")
+    }
+    else{
+        let nomprojet = prompt("Enter a project name. Don't forget it as you will need it to retrieve the result. Results are stored 48h only.", "project");
+	let pdbFiles1, pdbFiles2;
+	for (let i=0;i<listeBlobs.length;i++){
+	    if (listeBlobs[i].name == prot1pdb){
+		pdbFiles1=listeBlobs[i];
+	    }
+	    if (listeBlobs[i].name == prot2pdb){
+		pdbFiles2=listeBlobs[i];
+	    }
+	}
+	//
+	var content = 0;
+	/*fs.readFile(pdbFiles1, function read(err, data) {
+	    if (err) {
+		throw err;
+	    }
+	    content = data;
+	});*/
+	alert(content);
+	//	
+        if (nomprojet != null){
+	    var objectTest = {
+		"prot1": prot1pdb,
+		"prot2": prot2pdb,
+		"nomprojet": nomprojet
+	    };
+	    //alert(typeof pdbFiles1);
+	    //alert(objectTest['nomprojet']);
+	    alert("try to go to server");
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost:8080/subpart/proteins/',
+		dataType: "json",
+        	data: JSON.stringify(objectTest),
+		contentType: "application/json",		
+		processData: false,
+		success: function(msg){
+                    let answer = JSON.stringify(msg);
+                    answer = JSON.parse(answer);
+                    if (answer['msg'] == "Success"){
+			alert("Docking in progress, you can retrieve the result by clicking on 'Result Docking' and giving the project name.")
+                    }
+                    else{
+			alert(answer['msg']);
+                    }
+		},
+                error: function(jqXHR, textStatus, errorThrown) {
+        	    alert('error ' + textStatus + " " + errorThrown);
+                }
+            });
+        }
+    }
+}
+
+
+document.getElementById("load").addEventListener("click", getFile);
+document.getElementById("clear").addEventListener("click", clear);
+//REPRESENTATION
+document.getElementById("backbone").addEventListener("click", function(){
+    changeRepresentation("backbone", current_scheme);
+    // current_representation = "backbone";
+});
+document.getElementById("ballStick").addEventListener("click", function(){
+    changeRepresentation("ball+stick", current_scheme);
+    // current_representation = "ball+stick";
+});
+document.getElementById("cartoon").addEventListener("click", function(){
+    changeRepresentation("cartoon", current_scheme);
+    // current_representation = "cartoon";
+});
+
+document.getElementById("surface").addEventListener("click", function(){
+    changeRepresentation("surface", current_scheme);
+    // current_representation = "cartoon";
+});
+
+
+// COLOR
+document.getElementById("hydrophobicity").addEventListener("click", function(){
+    changeColor("hydrophobicity",  current_representation);
+  });
+document.getElementById("chainid").addEventListener("click", function(){
+    changeColor("chainid",  current_representation);
+  });
+document.getElementById("atomindex").addEventListener("click", function(){
+    changeColor("atomindex",  current_representation);
+  });
+
+document.getElementById("electrostatic").addEventListener("click", function(){
+    let tmp_scheme = current_scheme;
+    changeColor("electrostatic",  "surface");
+    current_scheme = tmp_scheme
+  });
+
+document.getElementById("spin").addEventListener("click", function(){
+  spin_flag = spin_flag === true ? false : true;
+  stage.setSpin(spin_flag);
+  });
+
+/*resets the value to address navigating away from the page
+and choosing to upload the same file */
+$('#hiddenInput').on('click touchstart' , function(){
+    $(this).val('');
+});
+
+
+//Trigger now when you have selected any file
+$("#hiddenInput").change(function(e) {
+  var elem = document.getElementById("hiddenInput");
+  pdb = elem.files[0];
+  loadPDB(pdb);
+});
+
+document.getElementById("dockDownload").addEventListener("click", function(){
+  var projet = prompt('Enter the project name you are looking for :', 'project');
+  if (projet != null){
+    $.ajax({
+        url:'http://localhost:8080/subpart/proteins/PDB/' + projet + '/macro.mac',
+        type:'GET',
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            alert('error ' + textStatus + " " + errorThrown + "\n\nThis project does not exist.");
+        },
+        success: function()
+        {
+          $.ajax({
+              url:'http://localhost:8080/subpart/proteins/PDB/' + projet + '/resultDocking.pdb',
+              type:'GET',
+              error: function(jqXHR, textStatus, errorThrown)
+              {
+                  alert("The docking is still in progress.");
+              },
+              success: function()
+              {
+                let formDownload = document.getElementById("formDownload");
+                formDownload.action = 'http://localhost:8080/subpart/proteins/PDB/' + projet + '/resultDocking.pdb';
+                var evt = document.createEvent("MouseEvents");
+                evt.initEvent("click", true, false);
+                let buttonDownload = document.getElementById("hiddenDownload");
+                buttonDownload.dispatchEvent(evt);
+              }
+          });
+        }
+    });
+  }
+});
